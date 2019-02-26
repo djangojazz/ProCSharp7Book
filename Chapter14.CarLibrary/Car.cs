4931d1b3b1c80887cd76a195146f9223b67a5353
Brett Morin <brett.morin@bluevolt.com>
2019-02-26 13:52:58 -0800

    Updated car library

--- Chapter14.CarLibrary/Car.cs
+++ Chapter14.CarLibrary/Car.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Chapter14.CarLibrary
 {
@@ -11,6 +12,14 @@ namespace Chapter14.CarLibrary
     {
         engineAlive,
         engineDead
+    }
+
+    public enum MusicMedia
+    {
+        musicCd,
+        musicTape,
+        musicRadio,
+        musicMp3
     }
     
     //The abstract base class in the hierarchy.
@@ -23,10 +32,14 @@ namespace Chapter14.CarLibrary
         protected EngineState engState = EngineState.engineAlive;
         public EngineState EngineState => engState;
         public abstract void TurboBoost();
+        public void TurnOnRadio(bool musicOn, MusicMedia mm) =>
+            MessageBox.Show(musicOn ? $"Jamming {mm}" : "Quiet time...");
 
-        public Car() { }
+
+        public Car() => MessageBox.Show("CarLibrary Version 2.0!");
         public Car(string name, int maxSp, int currSp)
         {
+            MessageBox.Show("CarLibrary Version 2.0!");
             PetName = name;
             MaxSpeed = maxSp;
             CurrentSpeed = currSp;
